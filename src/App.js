167d62d543124f138835649e9183b06a127d70e6
murodjon <luckyfreelancer997357847@gmail.com>
2020-08-30 20:38:12 +0500

    burger-builder add routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurderBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
+import Orders from "./containers/Orders/Orders";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout>
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/" component={BurgerBuilder} />
+        </Switch>
+      </Layout>
     </div>
   );
 }
